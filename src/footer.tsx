0b08b15281c58c4189f76dc304792c8427478c99
Oscar Garcia <oscaregarcialeon@gmail.com>
2024-12-13 08:54:38 -0400

    Creacion de  componente footer

--- src/footer.tsx
+++ src/footer.tsx
@@ -1,7 +1,19 @@
+import { Link } from "react-router-dom";
+import logo from './assets/logoC.png'
 
 const Footer = () => {
   return (
-   <footer></footer>
+   <footer className='px-[24] lg:px-[80px] z40 py-[32px] flex flex-col lg:flex-row gap-[42px] items-center justify-between w-full bg-white'>
+    <Link to='/' className="flex items-center gap-2.5 cursor-pointer">
+    <img src={logo} alt="logo" className="h-[18px] w-[115px]" />
+    </Link>
+    <div className="w-full text-center sm:flex items-center justify-center gap-[5px] be-vietnam-pro-regular md:text-[14px] md:leading-[18.9px] md:tracking-[-0.28px] text-[16px] leading-[23.2px] tracking-[0.32px]">
+      <p>Copyright 2024. Developed by: Oscar Garcia</p>
+    </div>
+    <Link to='' target="_blank" className="text-base leading-[21.6px] tracking-[-0.48px] bg-[#333] text-[#fff] rounded-full px-[18px] py-2.5 md:flex items-center justify-center min-w-max">
+      Connect Us
+    </Link>
+   </footer>
   );
 };
 
